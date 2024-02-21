704ca4506c3bb6fd27d7bf3903d97fefb697503d
Jonathan Oakley <h4yn0nnym0u5e@users.noreply.github.com>
2024-02-21 11:55:46 +0000

    Make startup.c match TD1.59

--- teensy4/startup.c
+++ teensy4/startup.c
@@ -69,14 +69,7 @@ void ResetHandler(void)
 #if 0
 	__asm__ volatile("dsb":::"memory");
 	__asm__ volatile("isb":::"memory");
-	ResetHandler2();
-	__builtin_unreachable();
-}
-
-__attribute__((section(".startup"), noinline, noreturn))
-static void ResetHandler2(void)
-{
-	unsigned int i;
+#endif
 	ResetHandler2();
 	__builtin_unreachable();
 }
@@ -114,11 +107,11 @@ static void ResetHandler2(void)
 	asm volatile("nop");
 	asm volatile("nop");
 #endif
+	// pin 13 - if startup crashes, use this to turn on the LED early for troubleshooting
 	//IOMUXC_SW_MUX_CTL_PAD_GPIO_B0_03 = 5;
 	//IOMUXC_SW_PAD_CTL_PAD_GPIO_B0_03 = IOMUXC_PAD_DSE(7);
 	//IOMUXC_GPR_GPR27 = 0xFFFFFFFF;
 	//GPIO7_GDIR |= (1<<3);
-	//GPIO7_DR_SET = (1<<3); // digitalWrite(13, HIGH);
 	//GPIO7_DR_SET = (1<<3); // digitalWrite(13, HIGH);
 
 	// Initialize memory
